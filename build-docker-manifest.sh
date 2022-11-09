90f65863603f62d13a728556e339730d5de5d613
Oliver Rahner <o.rahner@dke-data.com>
2022-11-09 23:37:54 +0100

    delete and recreate manifest

--- build-docker-manifest.sh
+++ build-docker-manifest.sh
@@ -1,8 +1,8 @@
 # make sure that local images are the most current ones
-docker pull oliverrahner/beenera-mqtt-bridge:latest-amd64
-docker pull oliverrahner/beenera-mqtt-bridge:latest-armv7
+docker pull docker.io/oliverrahner/beenera-mqtt-bridge:latest-amd64
+docker pull docker.io/oliverrahner/beenera-mqtt-bridge:latest-armv7
 
 # update manifest
-docker manifest create oliverrahner/beenera-mqtt-bridge:latest oliverrahner/beenera-mqtt-bridge:latest-amd64 oliverrahner/beenera-mqtt-bridge:latest-armv7
-docker manifest push oliverrahner/beenera-mqtt-bridge:latest
-
+docker manifest rm docker.io/oliverrahner/beenera-mqtt-bridge:latest
+docker manifest create docker.io/oliverrahner/beenera-mqtt-bridge:latest docker.io/oliverrahner/beenera-mqtt-bridge:latest-amd64 docker.io/oliverrahner/beenera-mqtt-bridge:latest-armv7
+docker manifest push docker.io/oliverrahner/beenera-mqtt-bridge:latest
