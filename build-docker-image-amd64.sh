f0e5a956874b2c1aad3da46052a8cd1121765e16
Oliver Rahner <o.rahner@dke-data.com>
2022-11-09 23:04:06 +0100

    add docker build scripts

---
+++ build-docker-image-amd64.sh
@@ -0,0 +1,4 @@
+docker buildx build --platform linux/amd64 -t docker.io/oliverrahner/beenera-mqtt-bridge:latest-amd64 .
+docker push docker.io/oliverrahner/beenera-mqtt-bridge:latest-amd64
+
+
---
+++ build-docker-image-armv7.sh
@@ -0,0 +1,2 @@
+docker buildx build --platform linux/arm/v7 -t docker.io/oliverrahner/beenera-mqtt-bridge:latest-armv7 .
+docker push docker.io/oliverrahner/beenera-mqtt-bridge:latest-armv7
